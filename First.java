3de019ae3588b3ae028d0e29fa1571ee68548579
Ravi Raj Singh <sraviraj774@gmail.com>
2023-06-16 13:14:29 +0530

    First Commit

---
+++ First.java
@@ -0,0 +1,18 @@
+public class First {
+    
+    /**
+     * @param arg
+     */
+    public static void main (String arg[]) {
+
+        System.out.println("First Program");
+
+        int a = 10;
+ 
+        int c = a + 5;
+
+        System.out.println(c);
+
+    }
+
+}
